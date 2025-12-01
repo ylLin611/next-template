40db74f98fa5de70612f01d99202c3fac26bbdc4
LIN <404@reach.com>
2025-12-01 16:12:32 +0800

    build: 📦️ e2b sandbox setup

---
+++ doc/e2b-sandbox.md
@@ -0,0 +1,35 @@
+# CLI
+
+## 安装CLI
+
+```bash
+npm install -g e2b
+```
+
+## 登录
+
+```bash
+e2b auth login
+```
+
+## 创建Template
+
+```bash
+e2b template build --name xxx --cmd xxx
+```
+
+注意：如果有 --cmd 需要在CMD下执行，如果是GIT BASH，路径会找GIT安装路径
+
+## 发布
+
+```bash
+e2b template publish -t team_Id
+```
+
+# USE
+
+## 安装
+
+```bash
+pnpm i @e2b/code-interpreter
+```
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
+import { Sandbox } from '@e2b/code-interpreter';
 import { createAgent, openai } from '@inngest/agent-kit';
 import { inngest } from './client';
+import { getSandbox } from './utils';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run('get-sandbox-id', async () => {
+      const sandbox = await Sandbox.create('next-workspace-test');
+      return sandbox.sandboxId;
+    });
+
     const writer = createAgent({
       name: 'writer',
       system: '你需要用一个词总结用户输入信息',
@@ -13,6 +20,12 @@ export const helloWorld = inngest.createFunction(
 
     const { output } = await writer.run(event.data.value);
 
-    return { message: output };
+    const sandboxUrl = await step.run('get-sandbox-url', async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = await sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 );
